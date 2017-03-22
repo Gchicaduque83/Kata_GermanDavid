360935a892617aca76b5f912d2afe1df6a476d89
David Ernesto Paniagua <de.paniagua@uniandes.edu.co>
2017-03-22 00:46:47 -0500

    Paso 1- Se corrige el método estadísticas para que pase la prueba con dos números (verde)

--- kata_GermanDavid/Estadisticas.py
+++ kata_GermanDavid/Estadisticas.py
@@ -4,5 +4,7 @@ class Estadisticas:
     def estadistica(self, cadena):
         if cadena == "":
             return 0
+        elif cadena.count(",")== 1:
+            return 2
         else:
             return 1
