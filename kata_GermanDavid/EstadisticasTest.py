6d8c73e946342a777843eadbf7245bc93af77fc8
GERMAN <gchicaduque@gmail.com>
2017-03-22 00:21:21 -0500

    Paso 1 - Se crea test para cuando la cadena es un solo numero (Rojo)

--- kata_GermanDavid/EstadisticasTest.py
+++ kata_GermanDavid/EstadisticasTest.py
@@ -5,3 +5,6 @@ from Estadisticas import Estadisticas
 class EstadisticasTest(TestCase):
     def test_estadistica(self):
         self.assertEqual(Estadisticas().estadistica(""),0,"Cadena vacia")
+
+    def test_estadistica1(self):
+        self.assertEqual(Estadisticas().estadistica("5"),1,"Un numero")
