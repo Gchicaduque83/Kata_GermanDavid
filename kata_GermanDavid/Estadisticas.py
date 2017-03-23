1c19087c5b0281222f40b74a1c2d88acd3bc1691
David Ernesto Paniagua <de.paniagua@uniandes.edu.co>
2017-03-23 00:41:17 -0500

    Paso 3- se corrige método estadísticas para que pase la prueba de máximo cuando la cadena tiene N elementos (Verde)

--- kata_GermanDavid/Estadisticas.py
+++ kata_GermanDavid/Estadisticas.py
@@ -5,13 +5,8 @@ class Estadisticas:
         numeros_vector = cadena.split(",")
         if cadena == "":
             return [0,0,0]
-        elif len(numeros_vector)== 1:
-            maximo = int(numeros_vector[0])
         else:
-            if int(numeros_vector[0]) > int(numeros_vector[1]):
-                maximo = int(numeros_vector[0])
-            else:
-                maximo = int(numeros_vector[1])
+           maximo = int(max(numeros_vector))
         elementos = len(numeros_vector)
         minimo = int(min(numeros_vector))
         return [elementos,minimo, maximo]
