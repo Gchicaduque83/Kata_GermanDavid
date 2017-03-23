8121e05012b67226f9efd3260d9bb958bb98c51a
GERMAN <gchicaduque@gmail.com>
2017-03-22 23:50:16 -0500

    Paso 2 - Se corrige el metodo estadisticas para que pase la prueba de minimo con dos numeros (Verde)

--- kata_GermanDavid/Estadisticas.py
+++ kata_GermanDavid/Estadisticas.py
@@ -2,11 +2,17 @@ __author__ = 'German - David'
 
 class Estadisticas:
     def estadistica(self, cadena):
+        numeros_vector = cadena.split(",")
         if cadena == "":
             return [0,0]
+        elif len(numeros_vector)==1:
+            minimo = int(numeros_vector[0])
         else:
-            elementos = len(cadena.split(","))
-            minimo = int((cadena.split(",")[0]))
-            return [elementos,minimo]
+            if int(numeros_vector[0])<int(numeros_vector[1]):
+                minimo = int(numeros_vector[0])
+            else:
+                minimo = int(numeros_vector[1])
+        elementos = len(numeros_vector)
+        return [elementos,minimo]
 
 
