4e14fd822b11bf31a2df2eca2059f766d3645dab
David Ernesto Paniagua <de.paniagua@uniandes.edu.co>
2017-03-23 00:06:18 -0500

    Paso 2- Se refactoriza el método estadisticas eliminando una operación sobrante y ajustando nombres de metodos de pruebas (Azul)

--- kata_GermanDavid/Estadisticas.py
+++ kata_GermanDavid/Estadisticas.py
@@ -5,8 +5,6 @@ class Estadisticas:
         numeros_vector = cadena.split(",")
         if cadena == "":
             return [0,0]
-        elif len(numeros_vector)==1:
-            minimo = int(numeros_vector[0])
         else:
             minimo = int(min(cadena.split(",")))
         elementos = len(numeros_vector)
--- kata_GermanDavid/EstadisticasTest.py
+++ kata_GermanDavid/EstadisticasTest.py
@@ -3,18 +3,18 @@ from unittest import TestCase
 from Estadisticas import Estadisticas
 
 class EstadisticasTest(TestCase):
-    def test_estadistica(self):
+    def test_estadistica_elementos(self):
         self.assertEqual(Estadisticas().estadistica("")[0],0,"Cadena vacia")
 
-    def test_estadistica1(self):
+    def test_estadistica_elemento1(self):
         cadena= "1"
         self.assertEqual(Estadisticas().estadistica(cadena)[0], len(cadena.split(",")),"Un numero")
 
-    def  test_estadistica2(self):
+    def  test_estadistica_elementos2(self):
         cadena= "1,2"
         self.assertEqual(Estadisticas().estadistica(cadena)[0], len(cadena.split(",")),"Dos numeros")
 
-    def  test_estadisticaN(self):
+    def  test_estadistica_elementosN(self):
         cadena= "1,2,3,4,5,6,7,8,9"
         self.assertEqual(Estadisticas().estadistica(cadena)[0], len(cadena.split(",")),"N numeros")
 
