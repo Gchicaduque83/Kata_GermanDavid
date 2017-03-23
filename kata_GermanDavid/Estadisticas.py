61fb8158b2099e2475126b40c92c217ead7fded9
David Ernesto Paniagua <de.paniagua@uniandes.edu.co>
2017-03-23 00:34:29 -0500

    Paso 3- Se corrige metodo estadisticas para que pase la prueba de máximo con dos números (Verde)

--- kata_GermanDavid/Estadisticas.py
+++ kata_GermanDavid/Estadisticas.py
@@ -5,10 +5,15 @@ class Estadisticas:
         numeros_vector = cadena.split(",")
         if cadena == "":
             return [0,0,0]
+        elif len(numeros_vector)== 1:
+            maximo = int(numeros_vector[0])
         else:
-            minimo = int(min(cadena.split(",")))
-            maximo = int(cadena.split(",")[0])
+            if int(numeros_vector[0]) > int(numeros_vector[1]):
+                maximo = int(numeros_vector[0])
+            else:
+                maximo = int(numeros_vector[1])
         elementos = len(numeros_vector)
+        minimo = int(min(numeros_vector))
         return [elementos,minimo, maximo]
 
 
